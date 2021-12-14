01fbbaccc1445379074ae6454ea99e2bd3b1086e
Damon <48176382+TOMORI00@users.noreply.github.com>
2021-12-14 15:40:48 +0800

    TEMP

--- src/components/KgScaleDisplay.vue
+++ src/components/KgScaleDisplay.vue
@@ -1,21 +1,49 @@
 <template>
-<div>Scale: {{scale}}</div>
+  <div>
+    <div style="display: inline-block;height: 300px;margin-left: 70px">
+      <a-slider v-model="current" :default-value="30" :tipFormatter="tipFormatter" tooltipPlacement="right" vertical/>
+    </div>
+  </div>
 </template>
 
 <script>
+import { Slider as ASlider, } from "ant-design-vue";
+
 export default {
   name: "KgScaleDisplay",
-  data(){
-    return{
+  data() {
+    return {
       scale: 1,
+      current: 50,
     };
   },
-  methods:{
-    setScale(scale){
+  components: {
+    ASlider,
+  },
+  watch: {
+    scale() {
+      this.emitScaleChangeEvent();
+    }
+  },
+  methods: {
+    tipFormatter() {
+      if (this.current >= 50) {
+        this.scale = (this.current - 50) / 50 * 7 + 1;
+        this.emitScaleChangeEvent();
+        return this.scale;
+      } else {
+        this.scale = 1 / ((50 - this.current) / 50 * 7 + 1);
+        this.emitScaleChangeEvent();
+        return this.scale;
+      }
+    },
+
+    setScale(scale) {
       this.scale = scale;
     },
 
-    emitScaleChangeEvent(){
+    emitScaleChangeEvent() {
+      console.log(this.scale);
       this.$emit("scale-change", this.scale);
     },
   },
