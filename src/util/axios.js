c216b5d39b9274c0b054b19aaf165dd34a843eee
mjh <m15256072859@163.com>
2021-12-20 11:06:08 +0800

    feat: 服务器版本

--- src/util/axios.js
+++ src/util/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const service = axios.create({
     // baseURL: "http://127.0.0.1:4523/mock/476309/"
-    baseURL: "http://localhost:8000"
+    baseURL: "http://118.195.232.175:8080/anigraph-backend"
 });
 
 service.interceptors.request.use((config) => {
